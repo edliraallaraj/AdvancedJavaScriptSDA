7f730246da035eb1a5665fea347cabc47bc58a04
Jballiu <108086733+Jballiu@users.noreply.github.com>
2023-02-19 12:02:32 +0100

    my first commit

--- index.js
+++ index.js
@@ -67,3 +67,16 @@ console.log("testtt");
 console.log(
   `Fourth student is ${students[3].fName} and his knowledge on the second language is ${students[3].languages.javaScript}`
 );
+
+let fourthStudent = students[3];
+console.log(`Fourth student is ${fourthStudent.fName} and his knowledge on the second language is ${fourthStudent.languages.javaScript
+}`)
+let secondStudent = students[1];
+secondStudent.cities.push("Elbasan");
+console.log(secondStudent.cities);
+function isGreat(){
+    if(fourthStudent.isGreat === false){
+        return "Aleksander is not great"
+    }
+}
+console.log(isGreat());
