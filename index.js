fb589fb03ddcbbfc8fe686ed1a2ee6f1b5b08ff8
flavio.alibeaj <flavio.alibeaj@gmail.com>
2023-02-19 12:02:35 +0100

    Vendosa kodin tim

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
+// const students = [
+//   {
+//     id: 1,
+//     fname: "Dienis",
+//     age: 23,
+//     cities: ["Tirane", "Vlore"],
+//     isGreat: false,
+//     languages: [
+//       {
+//         javascript: "good",
+//         python: "very good",
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     fname: "Flavio",
+//     age: 23,
+//     cities: ["Sarande", "Vlore"],
+//     isGreat: true,
+//     languages: [
+//       {
+//         javascript: "excellent",
+//         python: "very good",
+//       },
+//     ],
+//   },
+//   {
+//     id: 3,
+//     fname: "Dafina",
+//     age: 17,
+//     cities: ["Kruje", "Vlore"],
+//     isGreat: true,
+//     languages: [
+//       {
+//         javascript: "good",
+//         python: "excellent",
+//       },
+//     ],
+//   },
+//   {
+//     id: 4,
+//     fname: "Aleksander",
+//     age: 25,
+//     cities: ["Tirane", "Selanik"],
+//     isGreat: false,
+//     languages: [
+//       {
+//         javascript: "meeh",
+//         python: "nice",
+//       },
+//     ],
+//   },
+//   {
+//     id: 5,
+//     fname: "Egla",
+//     age: 26,
+//     cities: ["Tirane", "Berat"],
+//     isGreat: true,
+//     languages: [
+//       {
+//         javascript: "quite good",
+//         python: "not bad",
+//       },
+//     ],
+//   }
+// ];
+
 const students = [
   {
     id: 1,
@@ -56,14 +124,25 @@ const students = [
   },
 ];
 
-// Access the fourth student's Second language and
-//  give it a sentence like "Fourth student is...
-// and his second language is ...
+
+
+// Access the fourth student's second language and give it a sentence like :
+// Fourth student is ... and his second language is...
+let fourthStudent= students[3]
+fourthStudent.fName= students[3].fName
+console.log(`Fourth student is ${fourthStudent.fName} and his knowledge on the second language is ${fourthStudent.languages.javaScript}`)
+
 // Add another city on Flavio's cities
-// Create a function that shows if Aleksander is great, or not
+let flavio= students[1]
+flavio.cities.push("Fier")
+console.log(flavio.cities)
 
-console.log("testtt");
-
-console.log(
-  `Fourth student is ${students[3].fName} and his knowledge on the second language is ${students[3].languages.javaScript}`
-);
+// Create a function that shows if Aleksander is great or not
+function check(student){
+  if (student.isGreat === true){
+      console.log(true)
+  } else{
+      console.log(false)
+  }
+}
+check(flavio)
